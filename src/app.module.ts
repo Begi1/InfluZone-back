7c1304575384d092f3f4ec1ba4a3ca7f1b4d9ff3
Giorgi <giorgibegii@gmail.com>
2025-03-20 15:09:06 +0400

    mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'; // Import ConfigModule
 import { InfluencerModule } from './influencer/influencer.module';
 import { CompanyModule } from './company/company.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://giorgibegii:ebn4L1VbjPPHxW55@test.6kluo.mongodb.net/'),
+    ConfigModule.forRoot(), 
+    MongooseModule.forRoot(process.env.MONGO_URI), 
     InfluencerModule,
     CompanyModule,
   ],
